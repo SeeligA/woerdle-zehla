1b76cfe0d6f8ce3c5936de5674fb85799d5f23a0
Arndt S <arndtseelig@mt-g.com>
2019-02-14 11:31:43 +0100

    Add text box for output

--- GUI/gui.py
+++ GUI/gui.py
@@ -1,7 +1,8 @@
 import sys
 
+
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QFileDialog, QMainWindow, QWidget
+from PyQt5.QtWidgets import qApp, QApplication, QFileDialog, QMainWindow, QWidget
 from PyQt5.QtGui import QIcon
 
 
@@ -13,17 +14,24 @@ class MyWindow(QMainWindow):
 
 
     def initUI(self):
+
         uic.loadUi('MTChallenge.ui', self)
         self.setWindowTitle('Wördlezehla')
         self.setWindowIcon(QIcon('Icon.png'))
+        #self.QLayout.setContentsMargins(11,11,11,11)
         self.statusBar().showMessage('Ready')
 
         self.show()
+
 
     def browse_file():
         text = QFileDialog.getOpenFileName(filter = 'HTML-Datei (*.htm *.html) ;; SDLXLIFF-Datei (*.sdlxliff)')[0]
         w.input_file_line_edit.setText(text)
 
+
+    def run_sample():
+        my_list = [1, 2, 3]
+        w.textOutput.setText(str(my_list))
 
 
 
@@ -31,9 +39,10 @@ if __name__ == '__main__':
 
     app = QApplication(sys.argv)
     w = MyWindow()
-
+    w.sample_button.clicked.connect(MyWindow.run_sample)
     w.browse_button.clicked.connect(MyWindow.browse_file)
-
+    w.actionOpen_File.triggered.connect(MyWindow.browse_file)
+    w.actionExit.triggered.connect(qApp.quit)
 
 
     sys.exit(app.exec_())
