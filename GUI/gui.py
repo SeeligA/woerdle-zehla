f8199be5ebd5374c8ab018b7c877efdfb7d04c58
Arndt S <arndtseelig@mt-g.com>
2019-02-14 09:26:43 +0100

    Change to OOP style

--- GUI/gui.py
+++ GUI/gui.py
@@ -2,14 +2,21 @@ import sys
 
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QFileDialog, QMainWindow, QWidget
-
+from PyQt5.QtGui import QIcon
 
 
 class MyWindow(QMainWindow):
 
     def __init__(self):
         super(MyWindow, self).__init__()
+        self.initUI()
+
+
+    def initUI(self):
         uic.loadUi('MTChallenge.ui', self)
+        self.setWindowTitle('Wördlezehla')
+
+        self.show()
 
     def browse_file(self):
         text = QFileDialog.getOpenFileName(filter = 'HTML-Datei (*.htm *.html) ;; SDLXLIFF-Datei (*.sdlxliff)')[0]
@@ -22,9 +29,9 @@ if __name__ == '__main__':
 
     app = QApplication(sys.argv)
     w = MyWindow()
-    w.setWindowTitle('Wördlezehla')
+
     w.browse_button.clicked.connect(MyWindow.browse_file)
 
-    w.show()
+
 
     sys.exit(app.exec_())
