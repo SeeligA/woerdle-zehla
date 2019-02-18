3600197fc1f49deae085b9e58d646bfc580ba283
Arndt S <arndtseelig@mt-g.com>
2019-02-18 18:44:31 +0100

    Add plotting window
    When selecting the plot button a new window with an empty canvas opens

--- GUI/gui.py
+++ GUI/gui.py
@@ -3,13 +3,18 @@ import sys
 
 from PyQt5 import uic
 from PyQt5.QtWidgets import qApp, QApplication, QFileDialog, QMainWindow, QWidget
+
 from PyQt5.QtGui import QIcon
 
 import random # required only for dummy plot
 
-from matplotlib.backends.backend_qt5agg import FigureCanvasAgg as Canvas
-from matplotlib.figure import Figure # required only for dummy plot
+from matplotlib.backends.qt_compat import QtCore, QtWidgets, is_pyqt5
+from matplotlib.backends.backend_qt5agg import (
+        FigureCanvas, NavigationToolbar2QT as NavigationToolbar)
+from matplotlib.figure import Figure
+
 import numpy as np
+#from tutorial2 import ApplicationWindow
 #matplotlib.use('Qt5Agg')
 
 
@@ -36,6 +41,9 @@ class MyWindow(QMainWindow):
         text = QFileDialog.getOpenFileName(filter = 'HTML-Datei (*.htm *.html) ;; SDLXLIFF-Datei (*.sdlxliff)')[0]
         w.input_file_line_edit.setText(text)
 
+    def plot_results():
+        app = ApplicationWindow()
+        app.show()
 
     def run_sample():
         '''Dummy function for sampling'''
@@ -52,14 +60,31 @@ class MyWindow(QMainWindow):
         my_list2 = [6, 7, 8]
         w.textOutput.setText(str(my_list2))
 
+class ApplicationWindow(QtWidgets.QMainWindow):
+    '''Show results in new window
+    Source: https://matplotlib.org/gallery/user_interfaces/embedding_in_qt_sgskip.html
+    '''
+    def __init__(self):
+        super().__init__()
+        self._main = QtWidgets.QWidget()
+        self.setCentralWidget(self._main)
+        layout = QtWidgets.QVBoxLayout(self._main)
 
+        static_canvas = FigureCanvas(Figure(figsize=(5, 3)))
+        layout.addWidget(static_canvas)
+        self.addToolBar(NavigationToolbar(static_canvas, self))
+
+
+        self._static_ax = static_canvas.figure.subplots()
+        #t = np.linspace(0, 10, 501)
+        #self._static_ax.plot(t, np.tan(t), ".")
 
 
 if __name__ == '__main__':
 
     app = QApplication(sys.argv)
     w = MyWindow()
-    #w.plot_button.clicked.connect(MyWindow.plot_sample)
+    w.plot_button.clicked.connect(MyWindow.plot_results)
     w.submit_button.clicked.connect(MyWindow.submit_entry)
     w.calculate_button.clicked.connect(MyWindow.run_calculation)
     w.sample_button.clicked.connect(MyWindow.run_sample)
